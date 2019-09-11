435afb27a4f19e83553e51b483a5c316d526e4af
Marek Smatana <smatana13@stud.uniza.sk>
2019-09-11 19:22:08 +0200

    Commit

--- src/test/java/TestPath.java
+++ src/test/java/TestPath.java
@@ -2,18 +2,31 @@ import Logic.AStarAlgorithm;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 
 
 public class TestPath {
 
     @Test
-    public void doTest() {
+    public void findPath_whenThereIsPossiblePath_findsTheShortestPath() {
         String[] stringMaze = {""};
         AStarAlgorithm alg = new AStarAlgorithm(new char[][]{   {'.', '.', '.', '.', '#', '.', '.', '.'},
                                                                 {'.', 'S', '.', '.', '#', '.', '.', '.'},
                                                                 {'.', '.', '.', '.', '#', '.', '#', 'X'},
                                                                 {'.', '.', '.', '.', '.', '.', '#', '.'}});
         char[] path = {'d','r', 'r', 'd', 'r', 'r', 'u', 'u', 'r', 'r', 'd', '\u0000'};
+        char[] result = alg.getCharPath();
+        assertArrayEquals(path, result);
+    }
+
+    @Test
+    public void findPath_whenThereIsNoPath_returnsThereIsNoPathText() {
+        String[] stringMaze = {""};
+        AStarAlgorithm alg = new AStarAlgorithm(new char[][]{   {'.', '.', '.', '.', '#', '.', '.', '.'},
+                                                                {'.', 'S', '.', '.', '#', '.', '.', '#'},
+                                                                {'.', '.', '.', '.', '#', '.', '#', 'X'},
+                                                                {'.', '.', '.', '.', '.', '.', '#', '.'}});
+        char[] path = "There is no path.".toCharArray();
         char[] result = alg.getCharPath();
         assertArrayEquals(path, result);
     }
